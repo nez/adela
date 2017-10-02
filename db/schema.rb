182c0525a671028f2ba1492ac1154daca4e309e1
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-10-02 05:16:15 -0500

    Renames the landing_page column to data_dictionary

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821173137) do
+ActiveRecord::Schema.define(version: 20171002095310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,7 +99,7 @@ ActiveRecord::Schema.define(version: 20170821173137) do
     t.string   "mbox",                limit: 255
     t.string   "temporal",            limit: 255
     t.string   "spatial",             limit: 255
-    t.text     "landing_page"
+    t.text     "data_dictionary"
     t.string   "accrual_periodicity", limit: 255
     t.integer  "catalog_id"
     t.datetime "created_at"
@@ -258,15 +258,15 @@ ActiveRecord::Schema.define(version: 20170821173137) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "organization_id"
-    t.integer  'failed_attempts',        default: 0,  null: false
-    t.string   'unlock_token'
-    t.datetime 'locked_at'
+    t.integer  "failed_attempts",                    default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["organization_id"], name: "index_users_on_organization_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ['unlock_token'], name: "index_users_on_unlock_token", unique: true, using: :btree
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
   create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
