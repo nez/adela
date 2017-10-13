f62e08fd3a4ff952959f8f995c9b462a1e9ce7f5
KielRodriguez <kr.rdz.20@gmail.com>
2017-10-13 11:10:07 -0500

    fature:separate refined data to tasks

--- db/migrate/20170516173615_update_invalid_datasets_accrual_periodicity.rb
+++
@@ -1,5 +0,0 @@
-class UpdateInvalidDatasetsAccrualPeriodicity < ActiveRecord::Migration
-  def change
-    Dataset.where(accrual_periodicity: nil).update_all(accrual_periodicity: 'irregular')
-  end
-end
