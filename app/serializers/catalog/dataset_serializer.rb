eff837004a81ed0845837db4709b81cda4ae5677
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-11-06 10:36:15 -0600

    Replaces the distributions catalog conditions

--- app/serializers/catalog/dataset_serializer.rb
+++ app/serializers/catalog/dataset_serializer.rb
@@ -2,6 +2,6 @@ class Catalog::DatasetSerializer < DatasetSerializer
   has_many :distributions, root: :distribution, serializer: DistributionSerializer
 
   def distributions
-    object.distributions.select(&:issued?)
+    object.distributions.where(state: 'published')
   end
 end
